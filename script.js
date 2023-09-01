7a5d16436a649d29e525005932ab534f863ce751
Tanishq Gupta <113412672+hxtanishq@users.noreply.github.com>
2023-09-01 18:17:19 +0530

    Calculator using JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+let user_input = document.getElementById("user input");
+let button = document.querySelectorAll("button");
+
+button.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+
+    if (e.target.textContent === "C") {
+      user_input.innerHTML = "";
+    } else if (e.target.textContent === "X") {
+      user_input.innerHTML = user_input.innerHTML.slice(0, -1);
+    } else if (e.target.textContent === "=") {
+      user_input.innerHTML = eval(user_input.innerHTML);
+    } else {
+      user_input.innerHTML += e.target.textContent;
+    }
+  });
+});
